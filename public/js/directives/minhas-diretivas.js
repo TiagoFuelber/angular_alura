2f775f3082cd9a768ec274d2084c70bda9d7e2fe
Tiago Fuelber <tfuelber@gmail.com>
2017-09-06 19:07:51 -0300

    cria diretiva minhaFoto

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -9,4 +9,15 @@ angular.module('minhasDiretivas', [])
     ddo.templateUrl = "js/directives/meu-painel.html";
 
     return ddo;
+})
+.directive("minhaFoto", function() {
+    var ddo = {};
+    ddo.restrict = "AE";
+    ddo.scope = {
+        url: '@',
+        titulo: '@'
+    };
+    ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+
+    return ddo;
 });
